63db7b962ad100dfdcf40db35310affddd23b376
Divya Gupta <divygupta0319@gmail.com>
2024-12-20 17:16:10 +0530

    Showing username and email for logged in user, in secure endpoint

--- src/main/java/com/fastturtle/oauth2client/controllers/HomeController.java
+++ src/main/java/com/fastturtle/oauth2client/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package com.fastturtle.oauth2client.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HomeController {
@@ -12,7 +16,14 @@ public class HomeController {
     }
 
     @GetMapping("/secured")
-    public String secured() {
-        return "Hello, Secured!";
+    public String secured(Authentication authentication) {
+        if(authentication != null && authentication.getPrincipal() instanceof OAuth2User) {
+            OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+            String username = oAuth2User.getAttribute("name");
+            String email = oAuth2User.getAttribute("email");
+            return "Hello, "+ username + ", your mail id : " + email;
+        }
+
+        return "Hello secured. Unable to fetch user details.";
     }
 }
