d45cf06a50a7e54a31207fe1300b0a16f88bb7a4
divya0319 <divygupta0319@gmail.com>
2024-12-20 11:35:02 +0530

    Working on consuming OAuth2 custom server, with this OAuth2 client, still not working properly

---
+++ src/main/java/com/fastturtle/oauth2client/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.fastturtle.oauth2client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/public/**").permitAll()
+                                .requestMatchers("/favicon.ico", "/default-ui.css", "/js/**", "/css/**", "/images/**").permitAll()
+                        .anyRequest().authenticated()
+                        )
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/home", true)
+                        .failureUrl("/login?error=true") // Redirect to /login with error parameter on failure
+                );
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/fastturtle/oauth2client/controllers/OAuth2ClientController.java
@@ -0,0 +1,19 @@
+package com.fastturtle.oauth2client.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAuth2ClientController {
+
+    @GetMapping("/login/oauth2/code/custom-client")
+    public String handleOAuth2Redirect(@RequestParam("code") String code) {
+        return "Authorization code received: " + code;
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "Authorisation from OAuth2 server is successful";
+    }
+}
