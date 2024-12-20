81d8f99eb6b9fa634ac6d4b6eadfa89a8cc6f03c
divya0319 <divygupta0319@gmail.com>
2024-12-20 14:41:57 +0530

    Initial setup with Spring security basic config added, basic controller created, with open and secured endpoints, removed all properties for now

--- src/main/java/com/fastturtle/oauth2client/config/SecurityConfig.java
+++ src/main/java/com/fastturtle/oauth2client/config/SecurityConfig.java
@@ -3,25 +3,23 @@ package com.fastturtle.oauth2client.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize ->
-                        authorize
-                                .requestMatchers("/public/**").permitAll()
-                                .requestMatchers("/favicon.ico", "/default-ui.css", "/js/**", "/css/**", "/images/**").permitAll()
-                        .anyRequest().authenticated()
-                        )
-                .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("/home", true)
-                        .failureUrl("/login?error=true") // Redirect to /login with error parameter on failure
-                );
-
-        return http.build();
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(withDefaults())
+                .build();
     }
 }
